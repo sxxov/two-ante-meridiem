a908b60ec1ae393bda6c2e604972c35e6bbb11d2
jiaSheng <50891910+sxxov@users.noreply.github.com>
2025-11-10 17:55:40 +0800

    Correctly consolidate sets in `TaskSignal`

--- assets/lib-signal-TaskSignal.js
+++ assets/lib-signal-TaskSignal.js
@@ -6,16 +6,30 @@ import { Signal } from './lib-signal.js';
  * @template T
  */
 export class TaskSignal extends Signal {
+  /** @type {T | undefined} */
+  #setPrevious;
   #setScheduled = false;
   /** @override */
   set(/** @type {T} */ value) {
-    if (this.#setScheduled) return;
-    this.#setScheduled = true;
+    if (!this.#setScheduled) {
+      this.#setScheduled = true;
+      this.#setPrevious = this.get();
+    }
 
-    queueMicrotask(() => {
-      this.#setScheduled = false;
-      super.set(value);
-    });
+    this.value = value;
+
+    if (!this.#setScheduled)
+      queueMicrotask(() => {
+        this.#setScheduled = false;
+
+        const same = /** @type {typeof TaskSignal} */ (
+          this.constructor
+        ).compare(this.#setPrevious, this.get());
+        this.#setPrevious = undefined;
+
+        if (same) return;
+        super.trigger();
+      });
   }
 
   #triggerScheduled = false;
